603284ca36ba6ffe42fca9542bfc95a2d8cf9361
☆ NOLA .ᐟ <urlocalemorawr@gmail.com>
2026-05-06 15:11:36 +0000

    Done

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,5 +1,3 @@
-// This is a small program. There are only two sections. This first section is what runs
-// as soon as the page loads.
 $(document).ready(function () {
   render($("#display"), image);
   $("#apply").on("click", applyAndRender);
